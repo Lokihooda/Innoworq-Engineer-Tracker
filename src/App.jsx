f96ca7657fa5bb838aed8bce7f1bf0fe176bd442
Lokesh Hooda <vhooda31@gmail.com>
2026-06-18 15:14:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import EngineerPortal from './pages/EngineerPortal';
 import AdminDashboard from './pages/AdminDashboard';
 import './index.css';
